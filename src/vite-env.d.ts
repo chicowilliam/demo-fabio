bc570b472511a450b3ece57a6df36a445910b7c5
chicowilliam <viniciuswilliam91@gmail.com>
2026-05-12 23:09:15 -0300

    types(vite): add declaration modules for static asset imports

---
+++ src/vite-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+declare module '*.svg.svg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.png.png' {
+  const src: string;
+  export default src;
+}
