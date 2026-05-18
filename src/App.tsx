bdf22e92bf35bb381f27b70772c459be741cb6b2
chicowilliam <viniciuswilliam91@gmail.com>
2026-05-18 13:25:00 -0300

    fix(routing): restore /login route and wrap dashboard in ProtectedRoute

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { Suspense, lazy } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import AppShell from './components/AppShell';
+import ProtectedRoute from './components/ProtectedRoute';
 import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
 
 const DashboardPage = lazy(() => import('./pages/DashboardPage'));
 const SectorPage = lazy(() => import('./pages/SectorPage'));
@@ -14,9 +16,16 @@ function App() {
     <Routes>
       {/* Public Routes */}
       <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<Navigate to="/dashboard" replace />} />
+      <Route path="/login" element={<LoginPage />} />
 
-      <Route path="/dashboard" element={<AppShell />}>
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <AppShell />
+          </ProtectedRoute>
+        }
+      >
         <Route
           index
           element={
