5d015ebaef454b6a3956a0d10695851f0fe8d142
chicowilliam <viniciuswilliam91@gmail.com>
2026-05-18 13:25:10 -0300

    fix(auth): preserve full path (pathname + search + hash) on redirect to login

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -11,7 +11,7 @@ function ProtectedRoute({ children }: ProtectedRouteProps) {
   const location = useLocation();
 
   if (!isAuthenticated) {
-    return <Navigate to="/login" replace state={{ from: location.pathname }} />;
+    return <Navigate to="/login" replace state={{ from: `${location.pathname}${location.search}${location.hash}` }} />;
   }
 
   return children;
